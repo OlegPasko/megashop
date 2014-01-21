3852ed334247d16a316f36f5b56d85bb647f51ab
OlegPasko <olegpasko@gmail.com>
2014-01-21 20:08:02 +0200

    lesson 4

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,23 @@
 class ProductsController < ApplicationController
   def index
-    if params[:category]
-      @products = Product.where(category_id: params[:category])
+    if params[:sort].present?
+      column = params[:sort].split('_').first
+      order = params[:sort].split('_').last.upcase
     else
-      @products = Product.all
+      column = 'name'
+      order = 'ASC'
     end
+
+    @products = if params[:category].present?
+                  Product.where(category_id: params[:category]).order("#{column} #{order}").paginate(:page => params[:page], :per_page => 16)
+                elsif params[:search].present?
+                  Product.where("name LIKE ? OR description LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%").order("#{column} #{order}").paginate(:page => params[:page], :per_page => 16)
+                else
+                  Product.order("#{column} #{order}").paginate(:page => params[:page], :per_page => 16)
+                end
   end
+  #@posts = Post.paginate(:page => params[:page])
+
 
   def show
     @product = Product.find(params[:id])
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,9 @@ class Product < ActiveRecord::Base
   validates_numericality_of :price, greater_than_or_equal_to: 0
 
   mount_uploader :image, ProductImageUploader
+
+  def to_param
+    "#{id}-#{Russian::transliterate(name).parameterize}"
+  end
+
 end
